d7f7815e92228ad16a2ce12bde6cf4d4eab04022
Nastya <Nastya@123gmail.com>
2025-04-13 23:14:00 +0500

    Задание 3 черновики

--- Цикл задание 3 for.py
+++ Цикл задание 3 for.py
@@ -1,7 +1,11 @@
 i = int(input("Введите натуральное число n ≤ 9 : "))
 
-n = 0
+n = 1
 
-for x in range(1, i+1):
-    n = x*10+1
+for x in range(0, i):
+    n = str(n)
+    n = x +1
     print (n)
+    for y in range(0, i):
+        sss
+
