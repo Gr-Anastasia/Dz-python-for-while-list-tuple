ff7cb7f69f355e4f24f5dbda791040f4f726b0bb
Nastya <Nastya@123gmail.com>
2025-04-14 21:27:26 +0500

    Задание 3 кривой результат, но работает

--- Цикл задание 3 for.py
+++ Цикл задание 3 for.py
@@ -2,10 +2,9 @@ i = int(input("Введите натуральное число n ≤ 9 : "))
 
 n = 1
 
-for x in range(0, i):
-    n = str(n)
-    n = x +1
-    print (n)
-    for y in range(0, i):
-        sss
-
+for x in range(1, i+1):
+    x = ""
+    for y in range(1, i+1):
+        x += str(y)
+        print(x)
+    
